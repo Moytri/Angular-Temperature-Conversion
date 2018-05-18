decf309ded9454a04203002d151d80e763ebeb1b
moytri <moytritopa@gmail.com>
2018-05-18 11:22:21 -0700

    include app title

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component }      from '@angular/core';
+import {  MyRemoteService } from './app.myremoteservice';
+
+// This component consumes the re-usable service.
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [MyRemoteService]
+})
+
+export class AppComponent {
+   title = "App";
+
+   
+}
