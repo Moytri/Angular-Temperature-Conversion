9fb0cd724d2cbe317bd9651861e63c8749c038db
moytri <moytritopa@gmail.com>
2018-05-20 16:57:09 -0700

    modify route, include about and footer

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,14 +5,14 @@ import { Routes, RouterModule }  from '@angular/router';
 import { AppComponent } from './app.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { HomeComponent } from './home/home.component';
-
-
+import { AboutComponent } from './about/about.component';
 
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'feedback', component: FeedbackComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }, 
+  { path: 'about', component: AboutComponent },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
